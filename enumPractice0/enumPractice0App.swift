921ac12439deedd232a4b9311e5fa0083b3ee8a2
Wongi <hpurs95@gmail.com>
2022-07-28 15:32:09 +0900

    Initial Commit

---
+++ enumPractice0/enumPractice0App.swift
@@ -0,0 +1,17 @@
+//
+//  enumPractice0App.swift
+//  enumPractice0
+//
+//  Created by MacBook Air on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct enumPractice0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
